a0ea44533d4138b81596d1db66c203e25993b188
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-17 15:04:09 +0700

    [FEATURE] Add NovelRespositoryImpl

--- src/main/java/com/enigma/jpa_enigpus/repository/impl/NovelRepositoryImpl.java
+++ src/main/java/com/enigma/jpa_enigpus/repository/impl/NovelRepositoryImpl.java
@@ -1,4 +1,11 @@
 package com.enigma.jpa_enigpus.repository.impl;
 
-public class NovelRepositoryImpl {
+import com.enigma.jpa_enigpus.entity.Novel;
+import com.enigma.jpa_enigpus.repository.NovelRepository;
+
+public class NovelRepositoryImpl implements NovelRepository {
+    @Override
+    public void save(Novel novel) {
+
+    }
 }
