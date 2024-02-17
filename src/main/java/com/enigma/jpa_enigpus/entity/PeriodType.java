43b292ddd799b8d93b8579de5c1924b3d2fde27e
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-17 12:22:29 +0700

    [FEATURE] Add periodType Class

---
+++ src/main/java/com/enigma/jpa_enigpus/entity/PeriodType.java
@@ -0,0 +1,5 @@
+package com.enigma.jpa_enigpus.entity;
+
+public enum PeriodType {
+    WEEKLY, MONTHLY
+}
---
+++ src/main/java/com/enigma/jpa_enigpus/entity/magazine.java
@@ -0,0 +1,8 @@
+package com.enigma.jpa_enigpus.entity;
+
+public class magazine {
+    private int id;
+    private String title;
+    private int year;
+
+}
