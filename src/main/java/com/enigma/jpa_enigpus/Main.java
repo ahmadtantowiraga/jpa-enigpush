c9f34ca7bb68acaf0bc5416c3c67ba7aa259996c
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-17 12:58:18 +0700

    [FEATURE] Add magazine_mapping.xml

--- src/main/java/com/enigma/jpa_enigpus/Main.java
+++ src/main/java/com/enigma/jpa_enigpus/Main.java
@@ -1,5 +1,6 @@
 package com.enigma.jpa_enigpus;
 
+import com.enigma.jpa_enigpus.entity.Novel;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -8,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("jpa-enigpus");
         EntityManager em=emf.createEntityManager();
-
+        em.persist(new Novel());
         em.close();
         emf.close();
     }
