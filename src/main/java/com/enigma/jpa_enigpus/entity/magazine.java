5216498de91c5f7b8a02f09255290f6d69aae61d
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-02-17 12:24:10 +0700

    [FEATURE] add magazine class

--- src/main/java/com/enigma/jpa_enigpus/entity/magazine.java
+++ src/main/java/com/enigma/jpa_enigpus/entity/magazine.java
@@ -4,5 +4,47 @@ public class magazine {
     private int id;
     private String title;
     private int year;
+    private PeriodType period;
 
+    public magazine(int id, String title, int year, PeriodType period) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.period = period;
+    }
+
+    public magazine() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public PeriodType getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(PeriodType period) {
+        this.period = period;
+    }
 }
